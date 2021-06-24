70a3ea595e6dfbbe01b3675e4ad4d2e30816f4f8
sehaan <alsgk1123@gmail.com>
2021-06-24 22:00:44 +0900

    Change Layout

--- static/js/slide.js
+++ static/js/slide.js
@@ -1,17 +1,24 @@
 var labelIndex = 1;
 var fadeIndex = 1;
 
+
 function showLabels(n) {
     var i;
     var labels = document.getElementsByClassName("flip");
     if(n > labels.length) {labelIndex = 1}
     if(n < 1) {labelIndex = labels.length}
 
-    for (i = 0; i < labels.length; i++) {
-        labels[i].style.backgroundColor = "rgb(40, 189, 189)";
-    }
-
-    labels[labelIndex - 1].style.backgroundColor = "rgb(0, 128, 128)";
+    
+    labels[0].style.backgroundColor = "#E7F1FF";
+    labels[1].style.backgroundColor = "#C7DEFF";
+    labels[2].style.backgroundColor = "#8EBBFC";
+    labels[3].style.backgroundColor = "#4D87D9"; labels[3].style.color = "#FFFFFF";
+    labels[4].style.backgroundColor = "#1B58AD"; labels[4].style.color = "#FFFFFF";
+    labels[5].style.backgroundColor = "#003277"; labels[5].style.color = "#FFFFFF";
+    
+    
+    labels[labelIndex - 1].style.backgroundColor = "#FFFFFF";
+    labels[labelIndex - 1].style.color = "#003277";
 }
 
 function showFades(n) {
